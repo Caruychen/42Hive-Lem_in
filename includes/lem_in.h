f8de55a21427d1ab5f39eaad38ee0f8c453961a7
Caruy Chen <caruychen@gmail.com>
2022-06-07 15:30:02 +0300

    Added true false error defines

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:04:58 by cchen             #+#    #+#             */
-/*   Updated: 2022/06/07 14:48:46 by cchen            ###   ########.fr       */
+/*   Updated: 2022/06/07 15:19:16 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@
 # include "flow_edge.h"
 # include "flow_node.h"
 # include "flow_network.h"
+
+# define TRUE 1
+# define FALSE 0
+# define ERROR -1
 
 #endif
