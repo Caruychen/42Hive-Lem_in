ac7e6416403c7f7871366fa7b2e1cbfd03a72630
Caruy Chen <caruychen@gmail.com>
2022-06-07 15:30:22 +0300

    changed pointer level

--- includes/flow_node.h
+++ includes/flow_node.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:47:39 by cchen             #+#    #+#             */
-/*   Updated: 2022/06/07 14:48:36 by cchen            ###   ########.fr       */
+/*   Updated: 2022/06/07 14:58:44 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 typedef struct	s_flow_node {
 	char	*alias;
-	t_vec	*edges;
+	t_vec	edges;
 }				t_flow_node;
 
 #endif
