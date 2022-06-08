d136f6eb9154fd2d83c1d8b8950288183fb5a45b
Caruychen <caruychen@gmail.com>
2022-06-08 23:42:37 +0300

    flow node documentation

--- includes/flow_node.h
+++ includes/flow_node.h
@@ -6,10 +6,35 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:47:39 by cchen             #+#    #+#             */
-/*   Updated: 2022/06/08 22:12:19 by cchen            ###   ########.fr       */
+/*   Updated: 2022/06/08 23:42:08 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ *   Flow node represents the rooms of the ant nest network. Each node is an
+ *   element of the adjacency list implemented as the flow_network data type.
+ *   
+ *   The flow node data type:
+ *    ---------------
+ *   | ALIAS | EDGES | 
+ *    ---------------
+ *      |        |
+ *      |       \ /                   
+ *      |        --------------------------------
+ *      |       | edge_0 | edge_1 | edge_2 | ... | } --> t_vec
+ *      |        --------------------------------
+ *     \ /                            / \
+ *       -----------                   |
+ *      | char *str |                  DATA TYPE: t_flow_edge
+ *       -----------
+ *
+ *    The flow node's ALIAS simply holds a string that is the name of the room
+ *    The EDGES is a "bag" or vector (dynamic array) of edges, which are the
+ *    links to adjacent rooms.
+ *    The EDGES are of an iterable data type such as the vector, so they can be
+ *    easily scanned as part of the Breadth First Seach pattern in the 
+ *    edmunds-karp algorithm when identifying the paths with max-flow.
+*/
 #ifndef FLOW_NODE_H
 # define FLOW_NODE_H
 
