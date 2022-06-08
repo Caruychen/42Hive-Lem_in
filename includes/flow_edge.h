4c3c18e70e9c7aef585b84d701ac5bd077728ce4
Caruychen <caruychen@gmail.com>
2022-06-08 22:13:27 +0300

    added implementatino for whole edge list

--- includes/flow_edge.h
+++ includes/flow_edge.h
@@ -6,19 +6,19 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:06:08 by cchen             #+#    #+#             */
-/*   Updated: 2022/06/08 14:55:25 by cchen            ###   ########.fr       */
+/*   Updated: 2022/06/08 22:11:03 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FLOW_EDGE_H
 # define FLOW_EDGE_H
 
-#include <stdint.h>
+# include <stdint.h>
 
 typedef struct s_flow_edge
 {
-	int	from;
-	int	to;
+	int		from;
+	int		to;
 	uint8_t	flow: 1;
 }				t_flow_edge;
 
@@ -26,6 +26,10 @@ t_flow_edge	*edge_make(int from, int to);
 int			edge_other(t_flow_edge *edge, int node);
 int			edge_has_residual_capacity_to(t_flow_edge *edge, int node);
 void		edge_augment_flow_to(t_flow_edge *edge, int node);
-void		edge_free(t_flow_edge *edge);
+void		edge_free(t_flow_edge **edge);
+
+t_vec		*edge_list_make(t_vec *edge_list);
+int			edge_list_push(t_vec *edge_list, t_flow_edge *edge);
+void		edge_list_free(t_vec *edge_list);
 
 #endif
---
+++ src/edge_list.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   edge_list.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 21:35:05 by cchen             #+#    #+#             */
+/*   Updated: 2022/06/08 22:03:05 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+t_vec	*edge_list_make(t_vec *edge_list)
+{
+	return (vec_new(edge_list, 1, sizeof(t_flow_edge *)));
+}
+
+int	edge_list_push(t_vec *edge_list, t_flow_edge *edge)
+{
+	return (vec_push(&(node->edges), (void *) &edge));
+}
+
+void	edge_list_free(t_vec *edge_list)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < edge_list->len)
+		edge_free(&(edge_list->memory[edge_list->elem_size * index++]));
+	vec_free(edge_list);
+}
