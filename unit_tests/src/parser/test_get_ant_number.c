ae2b219859490b2a6ac3bcbf33d81e2082707e0f
cnysten <cnysten@student.hive.fi>
2022-07-01 09:24:14 +0300

    new unit test

--- unit_tests/src/parser/test_get_ant_number.c
+++ unit_tests/src/parser/test_get_ant_number.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 14:27:36 by cchen             #+#    #+#             */
-/*   Updated: 2022/06/27 21:23:48 by carlnysten       ###   ########.fr       */
+/*   Updated: 2022/07/01 09:23:21 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,28 @@ void	test_get_ant_number(void)
 {
 	t_parser	parser;
 	t_vec		network;
+	int			ret;
 
 	ft_printf("Testing get_ant_number: ");
 
-	parser = (t_parser){"1", NULL, ANT_NUMBER, NONE, 0, 0, 0, 0, 0, 0};
-	get_ant_number(&parser, &network);
+	parser = (t_parser){.line = "1", .stage = ANT_NUMBER};
+	ret = get_ant_number(&parser, &network);
 	assert(parser.n_ants == 1);
+	assert(ret == OK);
+
+	parser = (t_parser){.line = "0", .stage = ANT_NUMBER};
+	ret = get_ant_number(&parser, &network);
+	assert(parser.n_ants == 0);
+	assert(ret == OK);
+
+	parser = (t_parser){.line = "-1"};
+	ret = get_ant_number(&parser, &network);
+	assert(ret == ERROR);
+
+	parser = (t_parser){.line = "42424242"};
+	ret = get_ant_number(&parser, &network);
+	assert(parser.n_ants == 42424242);
+	assert(ret == OK);
 
 	ft_printf("OK\n");
 }
