81ac89933cc4d755ee2e26b146c5210f7e048c95
cnysten <cnysten@student.hive.fi>
2022-07-03 19:29:01 +0300

    working get_link unit test

--- unit_tests/src/parser/test_get_link.c
+++ unit_tests/src/parser/test_get_link.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 14:27:36 by cchen             #+#    #+#             */
-/*   Updated: 2022/07/01 17:11:55 by cnysten          ###   ########.fr       */
+/*   Updated: 2022/07/03 19:28:12 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	test_get_link(void)
 {
 	t_parser	parser;
 	t_vec		network;
+	t_flow_edge *edge1;
+	t_flow_edge *edge2;
+	t_flow_node	*node1;
+	t_flow_node	*node2;
 	int			ret;
 
 	ft_printf("Testing get_link: ");
@@ -28,6 +32,14 @@ void	test_get_link(void)
 		return ;
 	ret = get_link(&parser, &network);
 	assert(ret == OK);
+
+	node1 = vec_get(&network, 0);
+	node2 = vec_get(&network, 1);
+	edge1 = node_get(node1, 0);
+	edge2 = node_get(node2, 0);
+	assert(edge1 == edge2);
+	assert(edge1->to == edge2->to);
+	assert(edge1->from == edge2->from);
 	network_free(&network);
 
 	parser = (t_parser){.line = "", .stage = LINKS};
