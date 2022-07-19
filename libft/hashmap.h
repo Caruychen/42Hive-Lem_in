772b0f2e27888d1fc531e764d0a658462e8b2efa
Caruy Chen <caruychen@gmail.com>
2022-07-19 16:43:21 +0300

    added hashmap_insert.c

--- libft/hashmap.h
+++ libft/hashmap.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 11:18:35 by cchen             #+#    #+#             */
-/*   Updated: 2022/07/19 14:34:29 by cchen            ###   ########.fr       */
+/*   Updated: 2022/07/19 15:17:39 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define MSG_ERR_HASH_OVER "Error: Hash value out of bounds."
 # define MSG_ERR_HASHMAP_FULL "Error: Hashmap full, couldn't add element."
 # define DJB2_SEED 5281
+# define HASH_NEW_CAPACITY 2
 
 typedef struct s_entry
 {
@@ -37,7 +38,7 @@ int				hashmap_new(t_hashmap *dst);
 int				hashmap_new_with_capacity(t_hashmap *dst, size_t capacity);
 t_entry			*hashmap_entry(t_hashmap *src, char *key);
 int				*hashmap_get(t_hashmap *src, char *key);
-int				hashmap_insert(t_hashmap *dst, char	*key, int value);
+t_entry			*hashmap_insert(t_hashmap *dst, char	*key, int value);
 int				hashmap_try_insert(t_hashmap *dst, char *key, int value);
 int				hashmap_remove(t_hashmap *dst, char *key);
 void			hashmap_free(t_hashmap *src);
---
+++ libft/hashmap_insert.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hashmap_insert.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 14:35:07 by cchen             #+#    #+#             */
+/*   Updated: 2022/07/19 16:43:05 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ * Inserts a key-value pair into the hashmap
+ *
+ * If the map did not have this key present, a new entry is made and a pointer
+ * to the entry is returned.
+ *
+ * If the map did have this key present the value is updated. The key is not
+ * updated. It will return a pointer to the entry that was updated.
+ */
+
+#include "hashmap.c"
+
+static int	_guards(t_hashmap *dst, char *key)
+{
+	if (!dst || !key)
+		return (HASH_ERR);
+	if (!dst->capacity && hashmap_new_with_capacity(dst, HASH_NEW_CAPACITY) == HASH_ERR)
+		return (HASH_ERR);
+	if (!dst->entries)
+		return (HASH_ERR);
+	if (dst->len >= dst->capacity)
+		return (ft_putendl_fd(MSG_ERR_HASHMAP_FULL, 2), HASH_ERR);
+	return (HASH_OK);
+}
+
+static inline size_t _hash_index(size_t index, size_t capacity)
+{
+	return (index - (capacity * (index >= capacity)));
+}
+
+static t_entry	_new_entry(char *key, int value)
+{
+	t_entry	entry;
+
+	entry.key = ft_strdup(key);
+	if (!entry.key)
+		return (entry);
+	entry.value = value;
+	return (entry);
+}
+
+t_entry	*hashmap_insert(t_hashmap *dst, char *key, int value)
+{
+	size_t	index;
+	size_t	hash_index;
+	size_t	available_index;
+	size_t	end;
+	t_entry			*entry;
+
+	if (_guards(dst, key) == HASH_ERR)
+		return (NULL);
+	index = hashmap_hasher(dst, key);
+	end = dst->capacity + index;
+	available_index = dst->capacity;
+	while (index < end)
+	{
+		hash_index = _hash_index(index, dst->capacity);
+		entry = &(dst->entries[hash_index]);
+		if (entry->key && !ft_strcmp(entry->key, key))
+			return (entry->value = value, entry);
+		if (!entry->key && hash_index < available_index)
+			available_index = hash_index;
+		index++;
+	}
+	entry = &(dst->entries[available_index]);
+	*entry = _new_entry(key, value);
+	if (!entry->key)
+		return (NULL);
+	dst->len++;
+	return (entry);
+}
