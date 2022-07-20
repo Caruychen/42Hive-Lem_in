bc39c56f41429e93fb34f5f6a7784af6eede939e
Caruy Chen <caruychen@gmail.com>
2022-07-20 14:32:40 +0300

    fixed compiler errors for hashmap_find_slot

--- libft/hashmap_find_slot.c
+++ libft/hashmap_find_slot.c
@@ -6,10 +6,11 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 22:19:11 by cchen             #+#    #+#             */
-/*   Updated: 2022/07/20 11:09:13 by cchen            ###   ########.fr       */
+/*   Updated: 2022/07/20 14:32:02 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_string.h"
 #include "hashmap.h"
 
 size_t	hashmap_find_slot(t_hashmap *src, char *key)
@@ -17,12 +18,12 @@ size_t	hashmap_find_slot(t_hashmap *src, char *key)
 	size_t	index;
 	t_entry	entry;
 
-	index = hashmap_hasher(dst, key);
-	entry = dst->entries[index];
-	while (entry->key && ft_strcmp(entry->key, key))
+	index = hashmap_hasher(src, key);
+	entry = src->entries[index];
+	while (entry.key && ft_strcmp(entry.key, key))
 	{
 		index = (index + 1) % src->capacity;
-		entry = dst->entries[index];
+		entry = src->entries[index];
 	}
 	return (index);
 }
