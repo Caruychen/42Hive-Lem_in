20c6454675a131619e4e8352338dc5413fb0141c
cnysten <cnysten@student.hive.fi>
2022-07-20 08:59:49 +0300

    improved error handling in update_capacities

--- src/edmonds_karp.c
+++ src/edmonds_karp.c
@@ -59,8 +59,13 @@ static int	update_capacities(t_vec *network, t_edm_karp *ek, t_vec *path)
 	while (1)
 	{
 		parent = *(long *)vec_get(path, current);
+		if (!parent)
+			return (error(MSG_ERR_NULL_PARENT));
 		edge = node_get_edge_between(vec_get(network, parent), current);
-		edge_augment_flow_to(edge, current);
+		if (!edge)
+			return (error(MSG_ERR_NULL_EDGE));
+		if (edge_augment_flow_to(edge, current) == ERROR)
+			return (error(MSG_ERR_AUG_FLOW));
 		if (parent == ek->source_id)
 			break ;
 		current = parent;
