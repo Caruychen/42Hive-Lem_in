aa75c24ed1a00dbfd057ba9a22351fb071f1aef8
Caruy Chen <caruychen@gmail.com>
2022-07-20 14:04:38 +0300

    update hashmap_try_insert to report errors

--- libft/hashmap_try_insert.c
+++ libft/hashmap_try_insert.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 11:26:01 by cchen             #+#    #+#             */
-/*   Updated: 2022/07/20 11:38:56 by cchen            ###   ########.fr       */
+/*   Updated: 2022/07/20 11:46:08 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
  * Tries to insert a key-value pair into the map, and returns a pointer to
  * the entry.
  *
- * If the map alreaady had this key present, nothing is updated, and a NULL
- * is returned.
+ * If the map alreaady had this key present, nothing is updated. An error is 
+ * reported and a NULL is returned.
  */
 
+#include "ft_string.h"
 #include "hashmap.h"
 
 static int	_guards(t_hashmap *dst, char *key)
@@ -52,6 +53,6 @@ t_entry	*hashmap_try_insert(t_hashmap *dst, char *key, int value)
 	index = hashmap_find_slot(dst, key);
 	entry = &(dst->entries[index]);
 	if (entry->key)
-		return (NULL);
+		return (ft_putendl_fd(MSG_ERR_NO_KEY, 2), NULL);
 	return (_write_entry(entry, key, value));
 }
