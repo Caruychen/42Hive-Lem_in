64156b307a68273d74778c5b0bd9dca8e1ec97f9
cnysten <cnysten@student.hive.fi>
2022-07-25 10:43:25 +0300

    Revert "line and file macros added to error func"
    This reverts commit a6efe611cafb788d5923c964460cf49a0021f997.

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 11:13:07 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/07/22 11:48:56 by cnysten          ###   ########.fr       */
+/*   Updated: 2022/06/26 23:58:42 by carlnysten       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 
 int	error(char *msg)
 {
-	ft_putstr_fd(__FILE__, 2);
-	ft_putstr_fd(":", 2);
-	ft_putnbr_fd(__LINE__, 2);
-	ft_putstr_fd(" -> ", 2);
 	ft_putendl_fd(msg, 2);
 	return (ERROR);
 }
