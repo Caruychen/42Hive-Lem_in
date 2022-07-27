15777539140f75abd378acc54d1dbe2ba711c5d5
Caruychen <caruychen@gmail.com>
2022-07-28 00:23:17 +0300

    Refactor search

--- src/bfs_search.c
+++ src/bfs_search.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 19:50:43 by cchen             #+#    #+#             */
-/*   Updated: 2022/07/27 20:58:09 by cchen            ###   ########.fr       */
+/*   Updated: 2022/07/28 00:22:42 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,11 @@ static int	save_edge(t_flow_network *network, t_bfs_utils *bfs_utils,
 {
 	*(bfs_utils->trace.edge_to)[to] = edge;
 	bfs_utils->marked[to] = TRUE;
-	if (to == network->sink)
-	{
-		if (!bfs_utils->saturate_trace)
-			return (TRUE);
-		edge_list_push(&bfs_utils->trace.sink_edges, edge);
-		bfs_utils->marked[to] = FALSE;
-	}
-	else
-		queue_push(&bfs_utils->queue, &to);
-	return (FALSE);
+	if (to != network->sink)
+		return (queue_push(&bfs_utils->queue, &to), 0);
+	if (!bfs_utils->saturate_trace)
+		return (TRUE);
+	return (edge_list_push(&bfs_utils->trace.sink_edges, edge), FALSE);
 }
 
 static int	search_edges(t_flow_network *network, t_bfs_utils *bfs_utils,
@@ -43,7 +38,7 @@ static int	search_edges(t_flow_network *network, t_bfs_utils *bfs_utils,
 	{
 		edge = node_get(&node, index);
 		to = edge_other(edge, current);
-		if (!bfs_utils->marked[to]
+		if ((!bfs_utils->marked[to] || to == network->sink)
 			&& condition(edge, to, &network->adj_list)
 			&& save_edge(network, bfs_utils, to, edge))
 			return (TRUE);
@@ -52,12 +47,13 @@ static int	search_edges(t_flow_network *network, t_bfs_utils *bfs_utils,
 	return (FALSE);
 }
 
-int	search(t_flow_network *network, t_bfs_utils *bfs_utils)
+int	search(t_flow_network *network, t_bfs_utils *bfs_utils,
+		int (*condition)(t_flow_edge *, size_t, t_vec *))
 {
 	while (queue_has_next(&bfs_utils.queue))
 	{
-		search_edges(network, &queue, &edge_has_residual_capacity_to,
-			bfs_utils);
+		if (search_edges(network, &queue, condition, bfs_utils))
+			break;
 	}
 	return (marked[network->sink]);
 }
