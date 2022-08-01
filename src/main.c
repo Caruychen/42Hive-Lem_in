1ed7c00e8b2840bdd90aaf59fc45bc0182101aa7
Caruy Chen <caruychen@gmail.com>
2022-08-01 16:58:04 +0300

    incorporate solve into main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:05:44 by cchen             #+#    #+#             */
-/*   Updated: 2022/07/27 15:24:43 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/01 16:54:46 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 int	main(void)
 {
 	t_flow_network	network;
+	t_pathset		pathset;
 
 	if (network_init(&network) == ERROR)
 		return (error(MSG_ERR_NETWORK_INIT));
 	if (parse_input(&network) == ERROR)
+		return (network_free(&network), ERROR);
+	if (solve(&network, &pathset) == ERROR)
 		return (network_free(&network), ERROR);
 	network_free(&network);
 	return (0);
