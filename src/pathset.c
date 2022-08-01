b4cdc219779e845571aa651d3ac8352cc0628a5a
Caruy Chen <caruychen@gmail.com>
2022-08-01 16:59:52 +0300

    fixed error handling and compiler error

--- src/pathset.c
+++ src/pathset.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:12:34 by cchen             #+#    #+#             */
-/*   Updated: 2022/08/01 16:28:19 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/01 16:56:39 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	pathset_init(t_pathset *pathset, size_t n_paths, size_t n_ants)
 		if (vec_new(&path.nodes, 1, sizeof(size_t)) == ERROR)
 			return (pathset_free(pathset), ERROR);
 		if (vec_push(&pathset->paths, &path) == ERROR)
-			return (vec_free(&pathset->paths), ERROR);
+			return (ERROR);
 		n_paths--;
 	}
 	pathset->ants = n_ants;
@@ -39,7 +39,7 @@ int	pathset_fill(t_pathset *pathset, t_trace trace)
 	while (index < trace.sink_edges.len)
 	{
 		path = pathset_get(pathset, index);
-		if (path_populate(path, index, trace) == ERROR)
+		if (path_fill(path, index, trace) == ERROR)
 			return (ERROR);
 		index++;
 	}
