e402af6732b5de5b74ae397e66f416edf840659f
Caruy Chen <caruychen@gmail.com>
2022-08-02 15:01:44 +0300

    remove un needed line

--- src/path.c
+++ src/path.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:00:39 by cchen             #+#    #+#             */
-/*   Updated: 2022/08/02 14:25:54 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/02 14:59:43 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ int	path_fill(t_path *path, size_t index, t_trace trace, t_flow_network *network
 	if (!path || !trace.edge_to || !trace.sink_edges.memory)
 		return (ERROR);
 	edge = edge_list_get(&trace.sink_edges, index);
-	if (!edge)
-		return (ERROR);
 	while (edge)
 	{
 		if (vec_push(&path->nodes, network_get(network, edge->to)) == ERROR)
