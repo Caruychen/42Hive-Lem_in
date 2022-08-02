27ce0525e3e5fbe411e7a5f118d54fd7297a4fc1
Carl Nysten <nysten.carl@gmail.com>
2022-08-02 20:44:10 +0300

    quiet flag silences parser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:05:44 by cchen             #+#    #+#             */
-/*   Updated: 2022/08/02 20:25:12 by carlnysten       ###   ########.fr       */
+/*   Updated: 2022/08/02 20:30:21 by carlnysten       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
 		return (ft_putendl(MSG_USAGE), OK);
 	if (network_init(&network) == ERROR)
 		return (error(MSG_ERR_NETWORK_INIT));
-	if (parse_input(&network) == ERROR)
+	if (parse_input(&network, &options) == ERROR)
 		return (network_free(&network), ERROR);
 	if (solve(&network, &pathset) == ERROR)
 		return (network_free(&network), ERROR);
