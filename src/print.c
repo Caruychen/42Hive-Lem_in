273d32a919ab51505033d9caa3ef1a631680106d
Caruy Chen <caruychen@gmail.com>
2022-08-03 11:46:48 +0300

    minor tweaks

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 18:47:54 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/08/03 10:52:49 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/03 11:42:33 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,10 +61,10 @@ static void	send_ant(t_printer *printer)
 
 	update_move_prefix(printer);
 	i = 0;
-	while (i < printer->path->nodes.len - 1)
+	while (i < printer->path->nodes.len)
 	{
 		printer->move.len = printer->dash_id;
-		node = vec_get(&printer->path->nodes, printer->path->nodes.len - i - 2);
+		node = vec_get(&printer->path->nodes, printer->path->nodes.len - i - 1);
 		vec_append_str(&printer->move, node->alias);
 		line = vec_get(&printer->lines, printer->start_line + i);
 		vec_append_strn(line, printer->move.memory, printer->move.len);
@@ -80,14 +80,13 @@ static void	send_ant_wave(t_printer *printer, t_pathset *pathset)
 	i = 0;
 	while (i < pathset->paths.len)
 	{
-		printer->path = vec_get(&pathset->paths, i);
+		printer->path = vec_get(&pathset->paths, i++);
 		if (printer->path->ants > 0)
 		{
 			printer->path->ants--;
 			printer->ant_number++;
 			send_ant(printer);
 		}
-		i++;
 	}
 	printer->start_line++;
 }
@@ -99,9 +98,7 @@ int	print_solution(t_pathset *pathset)
 	if (printer_init(&printer, pathset) == ERROR)
 		return (printer_free(&printer), ERROR);
 	while (has_ants_to_send(pathset))
-	{
 		send_ant_wave(&printer, pathset);
-	}
 	vec_iter(&printer.lines, put_line);
 	return (printer_free(&printer), OK);
 }
