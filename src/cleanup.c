901d94f9fcc1dddd661975e6e0a333bd5c73362c
Caruy Chen <caruychen@gmail.com>
2022-08-03 11:00:55 +0300

    clean up main freeing code

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 11:13:07 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/06/26 23:58:42 by carlnysten       ###   ########.fr       */
+/*   Updated: 2022/08/03 10:58:04 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,11 @@ int	error(char *msg)
 {
 	ft_putendl_fd(msg, 2);
 	return (ERROR);
+}
+
+int	main_free(int res, t_flow_network *network, t_pathset *pathset)
+{
+	network_free(network);
+	pathset_free(pathset);
+	return (res);
 }
