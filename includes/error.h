d37464d88fbe50ca1d1e8a33e3b1806082f71be1
Caruy Chen <caruychen@gmail.com>
2022-08-09 15:14:33 +0300

    modify error messages

--- includes/error.h
+++ includes/error.h
@@ -6,22 +6,22 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 11:35:19 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/08/03 11:00:25 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/09 15:01:00 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
-# define MSG_ERROR_INV_FILE "File did not contain ant number, rooms or links."
-# define MSG_ERROR_INV_LINE "File contained an invalid line."
-# define MSG_ERROR_GNL "Could not get next line."
-# define MSG_ERROR_CHAR_ALIAS "Invalid character in room alias."
-# define MSG_ERROR_MALLOC_VLINK "Could not allocate memory in is_valid_link."
-# define MSG_ERROR_MOD "Invalid modification."
-# define MSG_ERR_NETWORK_INIT "Could not initialize flow network."
-# define MSG_ERR_HASH_INIT "Could not initialize hashmap."
-# define MSG_ERR_HASH_GET "Could not get entry from hashmap."
+# define MSG_ERROR_INV_FILE "ERROR: File did not contain ant number, rooms or links."
+# define MSG_ERROR_INV_LINE "ERROR: File contained an invalid line."
+# define MSG_ERROR_GNL "ERROR: Could not get next line."
+# define MSG_ERROR_CHAR_ALIAS "ERROR: Invalid character in room alias."
+# define MSG_ERROR_MALLOC_VLINK "ERROR: Could not allocate memory in is_valid_link."
+# define MSG_ERROR_MOD "ERROR: Invalid modification."
+# define MSG_ERR_NETWORK_INIT "ERROR: Could not initialize flow network."
+# define MSG_ERR_HASH_INIT "ERROR: Could not initialize hashmap."
+# define MSG_ERR_HASH_GET "ERROR: Could not get entry from hashmap."
 
 int	error(char *msg);
 int	main_free(int res, t_flow_network *network, t_pathset *pathset);
