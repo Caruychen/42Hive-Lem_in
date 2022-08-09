d7d2f1898c58de064731ee15debb8edf854b434d
Caruychen <caruychen@gmail.com>
2022-08-09 21:09:39 +0300

    added more error messages

--- includes/error.h
+++ includes/error.h
@@ -6,7 +6,7 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 11:35:19 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/08/09 18:31:23 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/09 20:47:49 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 # define MSG_ERROR_INV_FILE "ERROR: File missing ant number, rooms or links."
 # define MSG_ERROR_INV_LINE "ERROR: File contained an invalid line."
-# define MSG_ERROR_INV_LINK "Error: File contained an invalid link input"
+# define MSG_ERROR_INV_LINK "ERROR: File contained an invalid link input"
 # define MSG_ERROR_GNL "ERROR: Could not get next line."
 # define MSG_ERROR_CHAR_ALIAS "ERROR: Invalid character in room alias."
 # define MSG_ERROR_MALLOC_VLINK "ERROR: Memory allocation failed."
 # define MSG_ERROR_MOD "ERROR: Invalid modification."
+# define MSG_ERROR_MOD_DUP "ERROR: Duplicated modifier"
+# define MSG_ERROR_MOD_MISSING "ERROR: Missing start or end modifier"
 # define MSG_ERR_NETWORK_INIT "ERROR: Could not initialize flow network."
 # define MSG_ERR_HASH_INIT "ERROR: Could not initialize hashmap."
 # define MSG_ERR_HASH_GET "ERROR: Could not get entry from hashmap."
