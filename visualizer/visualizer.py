01e6a855b25737fb21094a2f70ad030f560b16ec
cnysten <cnysten@student.hive.fi>
2022-08-11 11:15:27 +0300

    visualizer parses lem-in parser output

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
-# import sys
+import sys
 import argparse
 import pygame
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    name: str
+    x: int
+    y: int
 
 class Visualizer:
 
@@ -17,25 +24,56 @@ class Visualizer:
     ANT_COLOR = (255, 198, 88)
 
     def __init__(self):
+        self.ants = 0
+        self.nodes = []
+        self.edges = []
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         pygame.display.set_caption("Lem-in Visualizer")
 
     def render(self):
         self.screen.fill(self.BG_COLOR)
 
+    def read_input(self):
+        self.ants = int(sys.stdin.readline().strip('\n'))
+        
+        while True:
+            line = sys.stdin.readline().strip('\n')
+            print(line)
+            if line[0] == '#':
+                continue
+            if not ' ' in line:
+                break
+            split = line.split(' ')
+            self.nodes.append(Node(split[0], int(split[1]), int(split[2])))
+
+        while True:
+            line = sys.stdin.readline().strip('\n')
+            print(line)
+            if len(line) == 0:
+                break
+            if line[0] == '#':
+                continue
+            if not '-' in line:
+                break
+            self.edges.append(line.split('-'))
+
+        print(self.nodes)
+        print(self.edges)
+
 def main():
     argparser = argparse.ArgumentParser(description = "Visualize your Lem-in.")
 
     visualizer = Visualizer()
-    running = True
+    visualizer.read_input()
 
+    running = True
     while running:
         visualizer.render()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == K_ESCAPE:
+                if event.key == pygame.K_ESCAPE:
                     running = False
 
         pygame.display.flip()
