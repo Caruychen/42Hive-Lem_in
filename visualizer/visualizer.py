c33e6d665634eef24bdd5c38f951f3aaeac09ebc
cnysten <cnysten@student.hive.fi>
2022-08-11 15:54:41 +0300

    visualizer draws nodes

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -15,6 +15,7 @@ class Visualizer:
 
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
+    PADDING = 20
     MAX_FPS = 60
     FONT_SIZE = 20
     BG_COLOR = (80, 43, 58)
@@ -22,16 +23,25 @@ class Visualizer:
     EDGE_COLOR = (255, 141, 109)
     NODE_COLOR = (255, 88, 93)
     ANT_COLOR = (255, 198, 88)
+    NODE_RADIUS = 5
 
     def __init__(self):
         self.ants = 0
+        self.max_x = 0
+        self.max_y = 0
         self.nodes = []
         self.edges = []
+        self.scale = 1
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         pygame.display.set_caption("Lem-in Visualizer")
 
+    def set_scale(self):
+        self.scale = (self.SCREEN_WIDTH - self.PADDING) / self.max_x
+
     def render(self):
         self.screen.fill(self.BG_COLOR)
+        for node in self.nodes:
+            pygame.draw.circle(self.screen, self.NODE_COLOR, (node.x * self.scale, node.y *self.scale), self.NODE_RADIUS)
 
     def read_input(self):
         self.ants = int(sys.stdin.readline().strip('\n'))
@@ -44,7 +54,13 @@ class Visualizer:
             if not ' ' in line:
                 break
             split = line.split(' ')
-            self.nodes.append(Node(split[0], int(split[1]), int(split[2])))
+            x = int(split[1])
+            if x > self.max_x:
+                self.max_x = x
+            y = int(split[2])
+            if y > self.max_y:
+                self.max_y = y
+            self.nodes.append(Node(split[0], x, y))
 
         while True:
             line = sys.stdin.readline().strip('\n')
@@ -65,6 +81,7 @@ def main():
 
     visualizer = Visualizer()
     visualizer.read_input()
+    visualizer.set_scale()
 
     running = True
     while running:
