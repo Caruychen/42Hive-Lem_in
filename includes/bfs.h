b5663ba89f775a3214c4e9b1d95365d01e3e3464
Caruy Chen <caruychen@gmail.com>
2022-08-17 16:07:35 +0300

    bfs.h documentation

--- includes/bfs.h
+++ includes/bfs.h
@@ -6,17 +6,45 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 18:35:57 by cchen             #+#    #+#             */
-/*   Updated: 2022/08/01 12:12:53 by cchen            ###   ########.fr       */
+/*   Updated: 2022/08/17 16:06:53 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BFS_H
 # define BFS_H
 
-# include "queue.h"
+/*
+ * BREADTH FIRST SEARCH 
+ *
+ * This BFS sub-function is made to perform basic network searching. Able to
+ * perform two types of search.
+ *
+ * 1. Single path search.
+ * 2. Multi path search.
+ *
+ * Single path search:
+ *   Used in the main edmunds-karp algorithm part. Searches for the
+ *   first, shortest, augmentable path. Terminates as soon as the end node 
+ *   is reached.
+ *
+ * Multi path search:
+ *   Used to extract a path set, containing all the augmented paths with 
+ *   flow to the end node. Searches until the entire network is traversed.
+ *   Will result in the edge_to and sink_edges array are saturated.
+*/
 
+# include "queue.h"
 # define SEARCH_END 1
 # define SEARCH_CONTINUE 0
+
+/*
+ * TRACE
+ * edge_to: An array with size N = Number of nodes. 
+ *    Indexes corresponds to the respective node's index. Each element is
+ *    the corresponding edge that points to the respective node.
+ *
+ * sink_edges: A vector holding all the edges pointing to the end node.
+*/
 
 typedef struct s_trace
 {
@@ -24,6 +52,7 @@ typedef struct s_trace
 	t_vec		sink_edges;
 }		t_trace;
 
+/* BFS UTILITIES */
 typedef struct s_bfs_utils
 {
 	int			*marked;
@@ -32,10 +61,13 @@ typedef struct s_bfs_utils
 	int			saturate_trace: 1;
 }		t_bfs_utils;
 
+/* BFS UTILITY FUNCTIONS */
 int		bfs_init(t_bfs_utils *bfs_utils, t_flow_network *network, int saturate);
 int		bfs_reset(t_bfs_utils *bfs_utils, t_flow_network *network);
+void	bfs_free(t_bfs_utils *bfs_utils);
+
+/* BFS SEARCH FUNCTION */
 int		bfs_search(t_flow_network *network, t_bfs_utils *bfs_utils,
 			int (*condition)(t_flow_edge *, size_t, t_vec *));
-void	bfs_free(t_bfs_utils *bfs_utils);
 
 #endif
